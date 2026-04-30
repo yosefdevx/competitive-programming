5412554fd8b9a6a2f9a3e3cd5047ec697f2a4c0c
Yosef A. <125744573+yosefdevx@users.noreply.github.com>
2026-04-30 08:11:11 +0300

    Time: 0 ms (100%), Space: 19.1 MB (99.09%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -1,27 +1,22 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        dict1 = {
-            1:["q","w","e","r","t","y","u","i","o","p"],
-            2:["a","s","d","f","g","h","j","k","l"],
-            3:["z","x","c","v","b","n","m"]            
+        rows = {
+            "1": "qwertyuiop",
+            "2": "asdfghjkl",
+            "3": "zxcvbnm"
         }
         ans = []
-        for item in words:
-            ref = 0
-            if item[0].lower() in dict1[1]:
-                ref = 1
-            elif item[0].lower() in dict1[2]:
-                ref = 2
-            elif item[0].lower() in dict1[3]:
-                ref = 3
-            else:
-                pass
-            flag = True
-            for char in item:
-                if char.lower() not in dict1[ref]:
-                    flag = False
-                    break
-            if flag:
-                ans.append(item)
+        for i in range(len(words)):
+            curr_row = set()
+            for j in range(len(words[i])):
+                curr_char = words[i][j].lower()
+                if curr_char in rows["1"]:
+                    curr_row.add("1")
+                elif curr_char in rows["2"]:
+                    curr_row.add("2")
+                else:
+                    curr_row.add("3")
+            if len(curr_row) == 1:
+                ans.append(words[i])
         return ans
         
